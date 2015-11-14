1eae1161d59c02c26b4bcab1d858104e48d068e3
nrm1 <2744128990@qq.com>
2015-11-14 10:51:49 +0800

    change_DM_R_add_sub_subu

--- src/DM.java
+++ src/DM.java
@@ -1,16 +1,19 @@
 public class DM{
 	//int only have 0 and 1
-	public  static int register[][] = new int[35][32];
+	public  static int register[][] = new int[38][32];
 	//register[32]=PC;register[33]=HI;register[34]=LO;
+	//register[35],register[36],register[37]are temporary registers;
+	//low bit 0;high bit 31;
 	public static int Overflow = 0;
 	public static int RAM[] = new int[10000];
 	public DM(){
-		for(int i = 0;i < 35;i++){
+		for(int i = 0;i < 38;i++){
 			for(int j = 0;j < 32;j++){
 				register[i][j] = 0;
 			}
 		}
 		register[5][1]=1;
+		register[5][31]=1;
 		register[6][2]=1;
 	}
 }
--- src/R.java
+++ src/R.java
@@ -12,7 +12,16 @@ public class R extends DM{
 			parameter3 -= 32;
 		}
 		if(instruction==74){
-			returnNumber = add(parameter1,parameter2,parameter3);
+			returnNumber = add(parameter1,parameter2,parameter3,0);
+		}
+		else if(instruction==75){
+			returnNumber = addu(parameter1,parameter2,parameter3,0);
+		}
+		else if(instruction==76){
+			returnNumber = sub(parameter1,parameter2,parameter3);
+		}
+		else if(instruction==77){
+			returnNumber = subu(parameter1,parameter2,parameter3);
 		}
 		else{
 			returnNumber = 1;
@@ -42,8 +51,7 @@ public class R extends DM{
 		}
 		return returnNumber;
 	}
-	private static int add(int parameter1,int parameter2,int parameter3){
-		int tempNumber = 0; 
+	private static int add(int parameter1,int parameter2,int parameter3,int tempNumber){
 		int temp30 = 0;
 		int calculatorNumber = 0;
 		for(int i=0;i<MaxBit;i++){
@@ -79,6 +87,111 @@ public class R extends DM{
 			else if(i==31){
 				Overflow = temp30^tempNumber;//XOR
 			}
+		}
+		return 0;
+	}
+	private static int addu(int parameter1,int parameter2,int parameter3,int tempNumber){ 
+		int calculatorNumber = 0;
+		for(int i=0;i<MaxBit;i++){
+			calculatorNumber=0;
+			if(tempNumber==1){
+				calculatorNumber++;
+			}
+			if(register[parameter2][i]==1){
+				calculatorNumber++;
+			}
+			if(register[parameter3][i]==1){
+				calculatorNumber++;
+			}
+			if(calculatorNumber==3){
+				register[parameter1][i]=1;
+				tempNumber=1;
+			}
+			else if(calculatorNumber==2){
+				register[parameter1][i]=0;
+				tempNumber=1;
+			}
+			else if(calculatorNumber==1){
+				register[parameter1][i]=1;
+				tempNumber=0;
+			}
+			else if(calculatorNumber==0){
+				register[parameter1][i]=0;
+				tempNumber=0;
+			}
+		}
+		return 0;
+	}
+	private static int sub(int parameter1,int parameter2,int parameter3){
+		if(register[parameter2][31]==0){
+			for(int i=0;i<32;i++){
+				register[36][i]=register[parameter2][i];
+			}
+		}
+		else{
+			register[36][31]=1;
+			for(int i=0;i<31;i++){
+				register[36][i]=1^register[parameter2][i];
+			}
+			addu(36,36,0,1);
+		}
+		if(register[parameter3][31]==1){
+			register[35][31]=0;
+			for(int i=0;i<31;i++){
+				register[35][i]=register[parameter3][i];
+			}
+		}
+		else{
+			register[35][31]=1;
+			for(int i=0;i<31;i++){
+				register[35][i]=1^register[parameter3][i];
+			}
+			addu(35,35,0,1);
+		}
+		add(37,36,35,0);
+		if(register[37][31]==1){
+			for(int i=0;i<31;i++){
+				register[37][i]=1^register[37][i];
+			}
+			addu(37,37,0,1);
+		}
+		for(int i=0;i < 32;i++){
+			register[parameter1][i] = register[37][i];
+		}
+		return 0;
+	}
+	private static int subu(int parameter1,int parameter2,int parameter3){
+		if(register[parameter2][31]==0){
+			for(int i=0;i<32;i++){
+				register[36][i]=register[parameter2][i];
+			}
+		}
+		else{
+			register[36][31]=1;
+			for(int i=0;i<31;i++){
+				register[36][i]=1^register[parameter2][i];
+			}
+			addu(36,36,0,1);
+		}
+		if(register[parameter3][31]==1){
+			register[35][31]=0;
+			for(int i=0;i<31;i++){
+				register[35][i]=register[parameter3][i];
+			}
+		}
+		else{
+			register[35][31]=1;
+			for(int i=0;i<31;i++){
+				register[35][i]=1^register[parameter3][i];
+			}
+			addu(35,35,0,1);
+		}
+		addu(37,35,36,0);
+		if(register[37][31]==1){
+			for(int i=0;i<31;i++){
+				register[37][i]=1^register[37][i];
+			}
+			addu(parameter1,37,0,1);
 		}
 		return 0;
 	}
